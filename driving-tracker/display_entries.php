92c5f15482d5e4227fac30b4ce35dfe1fc33dbb2
Abhishek shakya <abishek12344321@gmail.com>
2024-05-04 18:50:18 +0545

    started working on Webpage

--- driving-tracker/display_entries.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Read entries from file
-$entries = file("entries.txt");
-
-// Output each entry
-foreach ($entries as $index => $entry) {
-    echo "<div class='entry'><strong>" . ($index + 1) . ". Entry:</strong> " . $entry . "</div>";
-}
-?>
