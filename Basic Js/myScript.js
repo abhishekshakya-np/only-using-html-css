27a28bdf324067841eb4a45b2a07f96fff4e2c9b
Abhishek shakya <abishek12344321@gmail.com>
2024-06-02 16:39:28 +0545

    light bulb to turn on/off
    Click the light bulb to turn on/off with image tag and button the light.
    --> learned
    Quotes Handling:
    
    Use single quotes ' inside the onclick attribute to avoid conflicts with the double quotes " that wrap the entire attribute value.
    Function for Image Toggle:
    
    Added a changeImage function that toggles the image source when the image is clicked.

---
+++ Basic Js/myScript.js
@@ -0,0 +1,10 @@
+//document.getElementById("demo").innerHTML="Hello World!";
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("bulbon")) {
+        image.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+    } else {
+        image.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+    }
+}
+
