bd0f9542e9dee96c34251aaa398780ea9b634535
Abhishek shakya <abishek12344321@gmail.com>
2024-10-19 12:31:38 +0545

    Added basic HTML structure and CSS styling

--- Portfolio-Project/Js/gaming.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById("youtube-video");
-    video.src += "&autoplay=1"; // Append autoplay parameter to the video URL
-});
